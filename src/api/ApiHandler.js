c1d7b8fc328f83a96648c0ec01e6a2c4a2ce35a7
Sarwar-Shawon <sshawon95@gmail.com>
2024-03-30 03:30:42 +0000

    wip...

--- src/api/ApiHandler.js
+++ src/api/ApiHandler.js
@@ -55,8 +55,7 @@ export const Put = async (url, arg , ContentType) => {
 export const Get = async (url) => {
   try {
     const response = await api.get(url,{
-      withCredentials: true, 
-      credentials: 'include'
+      withCredentials: true,
     });
     // console.log('response', response.data);
     if (response.data.success) {
